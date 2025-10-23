82ebe91f8ae5d1c9e9aca8653585a3cedb6d7e7f
Sebastian-GOAT <sebastianrucabado123@gmail.com>
2025-10-23 18:15:10 +0200

    Fix: imports currected

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import Engine from './Engine.ts';
 
 import Sprite, { type SpriteOptions } from './Sprite.ts';
-import Rectangle, { type RectangleOptions } from './Rectangle.ts';
-import Oval, { type OvalOptions } from './Oval.ts';
-import RegularPolygon, { type RegularPolygonOptions } from './RegularPolygon.ts';
-import Pen, { type PenOptions } from './Pen.ts';
-import Text, { type TextOptions, type CanvasTextAlign, type CanvasTextBaseline } from './Text.ts';
-import ImageSprite, { type ImageSpriteOptions } from './ImageSprite.ts';
+import Rectangle, { type RectangleOptions } from './sprites/Rectangle.ts';
+import Oval, { type OvalOptions } from './sprites/Oval.ts';
+import RegularPolygon, { type RegularPolygonOptions } from './sprites/RegularPolygon.ts';
+import Pen, { type PenOptions } from './sprites/Pen.ts';
+import Text, { type TextOptions, type CanvasTextAlign, type CanvasTextBaseline } from './sprites/Text.ts';
+import ImageSprite, { type ImageSpriteOptions } from './sprites/ImageSprite.ts';
 
 import { setScale, setAspectRatio, canvas, ctx } from './canvas.ts';
 
